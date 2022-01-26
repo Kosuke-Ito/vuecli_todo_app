e5a51ae50716fd6eb6ad33d6a87a907382d93a5a
Kosuke Ito <keree.it.owl.uk@gmail.com>
2022-01-26 17:24:44 +0900

    詳細画面 追加

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -5,17 +5,30 @@
     <ul>
       <li 
         v-for="todo in todos"
-        v-bind:key="todo.id"
-        v-on:click="showTodo(todo)">
-          {{ todo.title }}
-          <span v-bind:style="changeDisplay(todo)">
-            {{todo.content}}
-          </span>
+        v-bind:key="todo.id">
+          <h3 v-on:click="showTodo(todo)">{{ todo.title }}</h3>
+          <div style="display:inline-block;" v-show="showingId === todo.id">
+            <form class="content">
+              <textarea type="text" style="display:block" ref="title" v-model="textInput" />
+              <div style="float: right;">
+                <button v-on:click="doEdit(textInput)">編集</button>
+                <button v-on:click="doRemove(todo)">削除</button>
+              </div>
+            </form>
+
+          </div>
       </li>
     </ul>
 
-    <h3>新しいタスクの追加</h3>
-    <form class="add-form" v-on:submit.prevent="doAdd">
+    <button class="toggle-form" v-on:click='toggle = !toggle'>
+      <span v-if="!toggle">
+        +
+      </span>
+      <span v-else>
+        x
+      </span>
+    </button>
+    <form class="add-form" v-on:submit.prevent="doAdd" v-show='toggle'>
       <div class="form-list">タイトル<input type="text" ref="title"></div>
       <div class="form-list">内容<input type="text" ref="content"></div>
       <button type="submit">追加</button>
@@ -26,32 +39,18 @@
 </template>
 
 <script>
-  let STORAGE_KEY = 'todos-vuejs-demo'
-  let todoStorage = {
-    fetch: function() {
-      let todos = JSON.parse(
-        localStorage.getItem(STORAGE_KEY) || '[]'
-      )
-      todos.forEach(function(todo, index) {
-        todo.id = index
-      })
-      todoStorage.uid = todos.length
-      return todos
-    },
-    save: function(todos) {
-      localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
-    }
-  }
-
+  import { todoStorage } from './TodoStorage.js'
 
   export default {
     // isShow: trueの時 コンテンツと編集と削除が表示される
     name: 'Todo',
-    props: { todo_app_title :String },
+    props: { todo_app_title: String },
     data: function () {
       return {
         todos: [],
-        isShow: false
+        toggle: false,
+        showingId: null,
+        textInput: ""
       }
     },
     created() {
@@ -67,8 +66,8 @@
     },
     methods: {
       doAdd: function () {
-        let title = this.$refs.title
-        let content = this.$refs.content
+        const title = this.$refs.title
+        const content = this.$refs.content
 
         if (!title.value.length || !content.value.length) {
           return
@@ -77,26 +76,74 @@
           id: todoStorage.uid++,
           title: title.value,
           content: content.value,
-          isShowDetail: false 
         })
         title.value = ""
         content.value = ""
       },
+      showTodo: function (todo) {
+        // ひらいているかどうか
+        const isCurrentTodoExpanded = this.showingId === todo.id
+        if (isCurrentTodoExpanded) {
+          // 開いていたら閉じる
+          this.showingId = null
+        } else {
+          // 閉じていたら開く
+          this.showingId = todo.id
+          this.textInput = todo.title + "\n" + todo.content
+        }
+      }
     }
   }
 </script>
 
 <style scoped>
-ul{
+ul {
   padding: 0 0 0 .5rem;
+}
+ul a {
+  color:blue;
+  text-decoration:none;
+}
+ul a:hover {
+  opacity: 0.3;
 }
 li {
   list-style-type: none;
-  margin-bottom: .5rem;
+  width: fit-content;
 }
-li > button{
-  font-size: .3rem;
-  padding: .05rem .25rem;
+li h3 {
+  margin: 1rem 0 0;
+}
+li h3:hover {
+  opacity: 0.3;
+}
+li div button {
+  margin-left: 0.5rem;
+  padding: 0.1rem 0.5rem;
+  font-size: 0.5rem;
+}
+li div .content input {
+  white-space: pre-wrap;
+  margin: .5rem 0;
+  padding: 0.5rem 5rem 0.5rem 1rem;
+  border: solid 1px;
+  border-radius: 5px;
+}
+li div p {
+  font-size: .8rem;
+  margin: .5rem 0;
+}
+.toggle-form {
+  margin: 1rem 0 0;
+  font-size: 1rem;
+  color:cornflowerblue;
+  width: 1.5rem;
+  height: 1.5rem;
+  background-color: unset;
+  border: none;
+}
+.toggle-form:hover {
+  opacity: 0.2;
 }
 .form-list{
   margin-bottom:.3rem;
---
+++ src/components/TodoStorage.js
@@ -0,0 +1,17 @@
+
+    const STORAGE_KEY = 'todos-vuejs-demo'
+    export const todoStorage = {
+      fetch: function() {
+        let todos = JSON.parse(
+          localStorage.getItem(STORAGE_KEY) || '[]'
+        )
+        todos.forEach(function(todo, index) {
+          todo.id = index
+        })
+        todoStorage.uid = todos.length
+        return todos
+      },
+      save: function(todos) {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+      }
+    }
